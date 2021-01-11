59d7abec9a23342fa107175e0757d672784b676c
fahad1226 <fahadbinmnr@gmail.com>
2021-01-12 00:24:44 +0600

    Basic stuff

---
+++ output/script.js
@@ -0,0 +1,18 @@
+"use strict";
+function addition(x, y) {
+    return x + y;
+}
+console.log(addition(12, 12));
+var person = {
+    name: "John",
+    age: 34,
+    passion: "Photography",
+    sport: "Cricket",
+};
+person.name = "Fahad";
+console.log(person);
+var friends = ["Ross"];
+friends.push("Joey", "Chandler");
+console.log(friends);
+var list;
+var names = [];
---
+++ src/script.ts
@@ -0,0 +1,26 @@
+function addition(x: number, y: number) {
+    return x + y;
+}
+
+console.log(addition(12, 12));
+
+const person = {
+    name: "John",
+    age: 34,
+    passion: "Photography",
+    sport: "Cricket",
+};
+
+person.name = "Fahad";
+
+console.log(person);
+
+const friends = ["Ross"];
+
+friends.push("Joey", "Chandler");
+
+console.log(friends);
+
+let list: string[];
+
+const names: string[] = [];
